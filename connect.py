c993d770a940e7a3247e5b6ecfcd232addda939c
LeVanToan-w <tl7378132@gmail.com>
2025-08-22 03:53:54 -0400

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,5 +1,11 @@
+import os
+
 def main():
     game = ConnectionAbortedError
     game.__call__ = "com.dts.freefireth"
+    if game == "com.dts.freefireth":
+        os.system("command")
+    else:
+        exit
     print({game})
     main()
